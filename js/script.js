14a9c098aa456cf0dc77712e17bdbaa34cd93667
Mohammad Ayman <60328501+MohammadAyman25-sudo@users.noreply.github.com>
2025-02-14 00:21:48 +0200

    web calculator-bugs fixed

--- js/script.js
+++ js/script.js
@@ -5,61 +5,64 @@ let numbers = $(".number");
 let equals = $(".evaluate");
 let opearation = $(".operation");
 
-numbers.each(function (){
-    $(this).click(function (){
-        opearation.each(function(){
-            $(this).prop("disabled", false);
-        });
-        let num = $(".result").text();
-        $(".result").text(`${num}${$(this).text()}`);
+numbers.each(function () {
+  $(this).click(function () {
+    opearation.each(function () {
+      $(this).prop("disabled", false);
     });
-});
-
-clear.click(function (){
-    $(".result").text("");
-    $(".operations").text("");
-    expression = "";
-    $("button").each(function (){
-        $(this).prop("disabled", false);
-    });
-    if ($(".result").text() == "") {
-      $(this).prop("disabled", true);
-    }
-});
-
-del.click(function (){
     let num = $(".result").text();
-    $(".result").text(num.slice(0, -1));
-    expression = "";
+    $(".result").text(`${num}${$(this).text()}`);
+  });
+});
+
+clear.click(function () {
+  $(".result").text("");
+  $(".operations").text("");
+  expression = "";
+  $("button").each(function () {
+    $(this).prop("disabled", false);
+  });
+  opearation.each(function () {
     if ($(".result").text() == "") {
       $(this).prop("disabled", true);
     }
+  });
 });
 
-opearation.each(function(){
+del.click(function () {
+  let num = $(".result").text();
+  $(".result").text(num.slice(0, -1));
+  expression = "";
+  opearation.each(function () {
     if ($(".result").text() == "") {
-        $(this).prop("disabled", true);
+      $(this).prop("disabled", true);
     }
-    $(this).click(function(){
-        expression = `${expression}${$(".result").text()}`;
-        if($(this).text() !== '=') {
-            expression = `${expression}${$(this).text()}`;
-        }
-        $(".result").text("");
-        $(".operations").text($(this).text());
-    });
+  });
 });
 
-equals.click(function(){
-    try {
-        let result = eval(expression);
-        $(".result").text(result);
+opearation.each(function () {
+  if ($(".result").text() == "") {
+    $(this).prop("disabled", true);
+  }
+  $(this).click(function () {
+    expression = `${expression}${$(".result").text()}`;
+    if ($(this).text() !== "=") {
+      expression = `${expression}${$(this).text()}`;
     }
-    catch(err) {
-        $(".result").text("Invalid Expression");
-    }
-    $("button").each(function() {
-        $(this).prop("disabled", true);
-    });
-    clear.prop("disabled", false);
+    $(".result").text("");
+    $(".operations").text($(this).text());
+  });
+});
+
+equals.click(function () {
+  try {
+    let result = eval(expression);
+    $(".result").text(result);
+  } catch (err) {
+    $(".result").text("Invalid Expression");
+  }
+  $("button").each(function () {
+    $(this).prop("disabled", true);
+  });
+  clear.prop("disabled", false);
 });
