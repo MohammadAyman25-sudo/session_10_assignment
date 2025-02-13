502af83bca20b1fc8fd104ceee7683504f8d6751
Mohammad Ayman <60328501+MohammadAyman25-sudo@users.noreply.github.com>
2025-02-14 00:10:32 +0200

    web calculator

---
+++ js/script.js
@@ -0,0 +1,59 @@
+let expression = "";
+let del = $(".del");
+let clear = $(".clear");
+let numbers = $(".number");
+let equals = $(".evaluate");
+let opearation = $(".operation");
+
+numbers.each(function (){
+    $(this).click(function (){
+        opearation.each(function(){
+            $(this).prop("disabled", false);
+        });
+        let num = $(".result").text();
+        $(".result").text(`${num}${$(this).text()}`);
+    });
+});
+
+clear.click(function (){
+    $(".result").text("");
+    $(".operations").text("");
+    expression = "";
+    $("button").each(function (){
+        $(this).prop("disabled", false);
+    });
+});
+
+del.click(function (){
+    let num = $(".result").text();
+    $(".result").text(num.slice(0, -1));
+    expression = "";
+});
+
+opearation.each(function(){
+    if ($(".result").text() == "") {
+        $(this).prop("disabled", true);
+    }
+    $(this).click(function(){
+        expression = `${expression}${$(".result").text()}`;
+        if($(this).text() !== '=') {
+            expression = `${expression}${$(this).text()}`;
+        }
+        $(".result").text("");
+        $(".operations").text($(this).text());
+    });
+});
+
+equals.click(function(){
+    try {
+        let result = eval(expression);
+        $(".result").text(result);
+    }
+    catch(err) {
+        $(".result").text("Invalid Expression");
+    }
+    $("button").each(function() {
+        $(this).prop("disabled", true);
+    });
+    clear.prop("disabled", false);
+});
